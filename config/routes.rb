e68a7b3fcc6681f6354c62f9949185bafe4dd0e9
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-04-10 14:38:32 +1000

    Update to use devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :events
+  # root 'devise/sessions#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
